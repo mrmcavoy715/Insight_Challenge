271a55c64101df5a0c7d8898d9e639e3528e0125
mrmcavoy715 <mrmcavoy715@gmail.com>
2018-06-10 14:49:02 -0400

    Updated readme

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # Insight Challenge
+
+## Initialization
+
+* Directories were created according to Insight's repo directory structure
+* Data gathered from SEC weblogs
+- download data - `wget http://www.sec.gov/dera/data/Public-EDGAR-log-file-data/2017/Qtr1/log20170101.zip` and `wget http://www.sec.gov/dera/data/Public-EDGAR-log-file-data/2017/Qtr1/log20170102.zip`
+- unzipped and catted together into log.csv
+- Log file is 3.7GB in size
+- Format is a comma-delimited file, where each line is a request. Parameters of interest are: ip, and the date and time
+
+## Methods - Detailed description of functions in sessionization.py
+
+Specifics of the code are available as comments in the main program - sessionization.py. The objective is to count the number of requests for a user, defined as a session.
+
+The code begins by accepting required parameters: input, period, and output. Input is the path to the log file. Period is the path to inactivity_period.txt, which determines how long a session is. Output is the path to the number of requests for a session.
+
+For extensibility, an object termed `logs` in the program is instantiated for the log file. This allows future work to allow input to be a directory, then adding a loop to generate an object for each log file in the directory.
+
+The main function of `logs` is called.
+
+The file is formatted so that the second line contains the earliest date in the file. Collecting this and formatting it as a datetime is done. Then the file is rewound back so the first request can be manipulated in the same way as other requests.
+
+Each request is a line in the file. For each request, the parameters of interest are collected. Due to format of the file, any ip that is equal to "ip" indicates a header row and should be skipped. The parameters of interest are: ip date, and time. 
+
+The time of the request is formatted as a datetime object by concatting the date and time of the request.
+
+Python dictionaries allow O(1) lookup times. It was determined this structure allowed for fast implementation and fast lookups. A dictionary termed `self.session_dict` holds the ip of the request, the start and end time of the request, a count for the number of requests, and an index. An index is necessary to preserve order the request came in to the order in the log file. The dictionary only contains in-progress sessions. If the dictionary does not contain a requests ip, that means it's a new session and is added to the dictionary. If a request's ip is found in the dictionary, then a session is still in progress and its count is incremented and its end time updated.
+
+As inactivity period is measured in seconds, it made sense to reduce the number of checks to once a second. This is done by knowing when a requests time is different than the previous request. Additionally, by checking for difference, the code can handle the unlikely event of a longer interval between requests. At this time, the function `to_output` is called followed by updating the previous time.
+
+`to_output` is written to check if a session's inactivity period has elapsed. It has optional parameters that allows for a check to occur both while reading over the file, and at the end of the file. The only way to know if a session has ended is to check all ongoing sessions and check their times. If the difference between the current time and a session's end time is greater than the inactivity period, or if it's the end of the log file, then the session has ended. If it's the end of the session, then this is written as the end time for the session. Then the session period is calculated as the difference between end time and start time. The count was already calculated in the main loop. Finally, this session is added to an array for deletion (python discourages deletion from a dictionary that is being looped over).
+
+As a requirement is to preserve order, the deletion array is sorted by start time and index (where index is line number in the log file). This allows users that have the same start time to be written to output in the same order they came in.
+
+Once the array has been sorted, the output file is written in the desired format. The session is deleted from the dictionary.
+
+Finally, once everything is complete, the input and output files are closed.
+
+## Results and future directions
+
+Using python dictionaries is a scalable way to handle large amounts of sessions with low overhead. By pruning the dictionary when a session expires, and reducing the number of checks to once a second, performance is gained. This method has trouble scaling when the inactivity period becomes large, as the memory required to hold the dictionary grows.
+
+On the author's PC, with an inactivity period of two seconds and 3.7GB of log data, took about 800 seconds ~ 13 minutes and the dictionary contained at most 207 active sessions. This is reasonable but doesn't scale very well. For example a month of log data would take about three hours. As order reading from the file is important, it is difficult to concieve of a distributed manner to solve the prompt. A simple way of scaling is to keep log files per day and distribute to multiple computers at once. This will reduce the total time to a single day, about 6.5 minutes. However this fails to account for sessions that overlap days. If order is relaxed to only time, then it's feasible a map reduce could be used. An initial attempt would start with mapping a key-value where the key is a tuple of user-time and a value of one. Then a reduce to sum the number of requests. As common implementations of reduce sort the output, this can then be followed by iterating over the output in a similar manner to the dictionary method. Performance is gained by reducing the size of the data iterating over. However this is not significantly different from the current method. 
+
--- insight_testsuite/temp/src/sessionization.py
+++ insight_testsuite/temp/src/sessionization.py
@@ -1,7 +1,8 @@
 '''
-Init sessionization.py
+progam: sessionization.py
+author: Matthew McAvoy
 
-Takes data from input/log.csv, streams and calculates user sessions
+Use: Solution to Insight's data engineering challenge
 '''
 
 import argparse
@@ -9,83 +10,112 @@ import datetime
 from timeit import timeit
 
 
-class logIterable(object):
+class userLogs(object):
 
     def __init__(self, parser):
+        '''
+        userlogs is a class that loads a log file, operates on it to 
+        count the number of requests for a user per session, and 
+        writes it to output
+
+        Definitions
+          self.args: accepts inputs from the command line
+
+          self.data: opens the input log file
+
+          self.output: opens writable output file
+
+          self.inactivity_period: value determining inactivity period
+
+          self.user_dict: dict of dict. First key is user, 
+            second key are various parameters for that user
+
+          self.del_user: array for users that are ready to be deleted
+            from self.user_dict
+
+          self.max_users: largest number of  users in self.user_dict
+            at one point during program run
+        
+        '''
 
         self.args = parser.parse_args()
-        #print("input_path", self.args.input)
-        #print("output path", self.args.output)
-        # parameters
-        # ip,date,time,zone,cik,accession,extention,code,size,idx,norefer,noagent,find,crawler,browser
         self.data = open(self.args.input)
         self.output = open(self.args.output, 'w')
-        self.inactivity_period = int(open(self.args.period).readline())
-        print("inactivity period", self.inactivity_period)
+        self.inactivity_period_file = open(self.args.period)
+        self.inactivity_period = int(self.inactivity_period_file.readline())
 
-        self.user_dict = {}
-        self.del_user = []
-        self.max_users = 0
-
-
-    def __iter__(self):
-
-        #line = self.data.readline()
-        #yield print(line.split(',')[0:2])
-        pass
+        self.session_dict = {}
+        self.del_session = []
+        self.max_sessions = 0
 
     def to_output(self, current_time, last_line = False):
+        '''
+        Check all users whether their session has ended. If it has, add 
+        that user's dict as one to write to output and them remove from 
+        the operating dict.
+        '''
 
-        for user in self.user_dict:
+        for session in self.session_dict:
 
-            #time_delta = self.user_dict[user]['end_time'] - self.user_dict[user]['start_time']
-            time_delta = current_time - self.user_dict[user]['end_time']
+            # time_delta is time difference between session's last request
+            # and the current time
+            time_delta = current_time - self.session_dict[session]['end_time']
             time_delta = int(time_delta.total_seconds())
             
-
+            # if the session has passed the inactivity period, or it's the
+            # end of the file, update the session's period of activity
+            # (time_delta_session), and add to array.
             if time_delta > self.inactivity_period or last_line == True:
                 if time_delta > self.inactivity_period and last_line == True:
-                    self.user_dict[user]['end_time'] = current_time
-                time_delta_user = self.user_dict[user]['end_time'] - self.user_dict[user]['start_time']
-                time_delta_user = int(time_delta_user.total_seconds()) + 1
-                print("time delta for user", time_delta, time_delta_user, user)
-                self.user_dict[user]['time_delta'] = time_delta_user
-                self.del_user.append(self.user_dict[user])
+                    self.session_dict[session]['end_time'] = current_time
+                time_delta_session = self.session_dict[session]['end_time'] - self.session_dict[session]['start_time']
+                time_delta_session = int(time_delta_session.total_seconds()) + 1
+                #print("time delta for session", time_delta, time_delta_session, session)
+                self.session_dict[session]['time_delta'] = time_delta_session
+                self.del_session.append(self.session_dict[session])
                     
-                    
-                
+        # sort array based on session's start time and order encountered
+        self.del_session = sorted(self.del_session, key=lambda key: (key['start_time'], key['index']))
+        #print("current time", current_time, "will delete", len(self.del_session), "number of sessions")
 
-        self.del_user = sorted(self.del_user, key=lambda key: (key['start_time'], key['index']))
-        print("current time", current_time, "will delete", len(self.del_user), "number of users")
-
-        for d in self.del_user:
-            user = d['ip']
-            print("deleting user", user, self.user_dict[user]['start_time'], self.user_dict[user]['end_time'], self.user_dict[user]['count'], self.user_dict[user]['index'])
-            output_line = user + ',' + self.user_dict[user]['start_time'].strftime("%Y-%m-%d %H:%M:%S") + ',' + self.user_dict[user]['end_time'].strftime("%Y-%m-%d %H:%M:%S") + ',' + str(self.user_dict[user]['time_delta']) + ',' + str(self.user_dict[user]['count']) + '\n'
+        # for each ending session, write to output and delete from dict
+        for d in self.del_session:
+            session = d['ip']
+            #print("deleting session", session, self.session_dict[session]['start_time'], self.session_dict[session]['end_time'], self.session_dict[session]['count'], self.session_dict[session]['index'])
+            output_line = session + ',' + self.session_dict[session]['start_time'].strftime("%Y-%m-%d %H:%M:%S") + ',' + self.session_dict[session]['end_time'].strftime("%Y-%m-%d %H:%M:%S") + ',' + str(self.session_dict[session]['time_delta']) + ',' + str(self.session_dict[session]['count']) + '\n'
             #print("ending session", output_line)
             self.output.write(output_line)
-            del self.user_dict[user]
+            del self.session_dict[session]
 
-        self.del_user = []
+        # now that all ending session's are done, remove from list of
+        # sessions to delete
+        self.del_session = []
         
     
-    def open_file(self):
-
+    def main(self):
+        '''
+        Main program that reads log file, gathers important parameters
+        from each request.
+        '''
+        
         # open the log file
         with open(self.args.input) as f:
 
-            # read the first line to get the first date
+            # read the first line to get the first date, then point
+            # the file back to the start so the first request
+            # isn't missed
             first_line_pointer = f.tell()
             first_line = f.readline()
             second_line = f.readline()
             previous_time = datetime.datetime.strptime(' '.join(second_line.split(',')[1:3]), "%Y-%m-%d %H:%M:%S")
-            
-            print("start_time", previous_time)
             f.seek(first_line_pointer)
+            print("first log time", previous_time)
 
-            # iterate over logs
+            # iterate over lines in the log file
             for i, line in enumerate(f):
 
+                # parameters
+                # ip,date,time,zone,cik,accession,extention,code,size,idx,norefer,noagent,find,crawler,browser
                 # split each log to get the ip and date-time
                 line_split = line.split(',')
                 ip = line_split[0]
@@ -100,55 +130,65 @@ class logIterable(object):
                 current_time = datetime.datetime.strptime(date + ' ' + time, "%Y-%m-%d %H:%M:%S")
 
                 # add a new user or returning user
-                if ip not in self.user_dict:
-                    self.user_dict[ip] = { 'ip': ip, 'start_time': current_time, 'end_time': current_time, 'count': 1, 'index': i }
-                # update users time and count if user is active
+                if ip not in self.session_dict:
+                    self.session_dict[ip] = { 'ip': ip, 'start_time': current_time, 'end_time': current_time, 'count': 1, 'index': i }
+                # update sessions time and count if session is active
                 else:
-                    self.user_dict[ip]['end_time'] = current_time
-                    self.user_dict[ip]['count'] += 1
+                    self.session_dict[ip]['end_time'] = current_time
+                    self.session_dict[ip]['count'] += 1
 
                 # if a second has passed, call to_output to check elapsed time
-                # for all users
                 if (current_time - previous_time).total_seconds() > 0:
                     
-                    # self check number of items in dict at any certain time
-                    user_count = len(self.user_dict)
+                    # self check to know max number of sessions
+                    # in self.session_dict
+                    session_count = len(self.session_dict)
                     
-                    if user_count > self.max_users:
-                        self.max_users = user_count
+                    if session_count > self.max_sessions:
+                        self.max_sessions = session_count
                     
                     self.to_output(current_time)
-                    
+
+                    # update previous time
                     previous_time = current_time
-        print("last time", previous_time)
-        # Write remaining users at end
+        
+        # Once log file has reached the end, write all
+        # remaining active sessions
         self.to_output(previous_time, last_line=True)
-                
-        #print("largest number of items in self.user_dict", self.max_users)
+
+        print("last log time", previous_time)        
+        print("largest number of items in self.session_dict", self.max_sessions)
         
                     
     def close_file(self):
 
         self.data.close()
+        self.inactivity_period_file.close()
         self.output.close()
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="""
+    Help page for sessionization.py program
+
+    Requires three inputs in this order
+      * an input log file
+      * a file that defines the period
+      * an output file
     """, formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument("input", type=str, nargs='?',  help="Path to input data")
-    parser.add_argument("period", type=str, nargs='?',  help="Path to input data")
-    parser.add_argument("output", type=str, nargs='?', help="Path to output data")
-    
-    start = logIterable(parser)
-    print(timeit(lambda: start.open_file(), number = 1))
-    start.close_file()
+    parser.add_argument("input", type=str, nargs='?',  help="Path to input file")
+    parser.add_argument("period", type=str, nargs='?',  help="Path to inactivity_period file")
+    parser.add_argument("output", type=str, nargs='?', help="Path to output file")
+
+    # instantiate logs object
+    logs = userLogs(parser)
+
+    # call main function
+    print("Starting sessionization.py program to calculate number of requests by a user during a session")
+    print(timeit(lambda: logs.main(), number = 1))
+
+    # close files before exiting
+    logs.close_file()
 
 
-    '''
-    Three map reduces?
-    First: map ip-second
-    Second: ip
-    Third: sort
-    '''
